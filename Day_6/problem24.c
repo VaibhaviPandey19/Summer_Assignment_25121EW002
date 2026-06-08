367ff29cb4212c8c259496a29aee334e285c8068
VaibhaviPandey19 <25121ew002@glbitm.ac.in>
2026-06-08 16:08:42 +0530

    Day_6 assignment is uploaded

---
+++ Day_6/problem24.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int x, n;
+    long long result = 1;
+    printf("Enter x and n: ");
+    scanf("%d %d", &x, &n);
+    for (int i = 0; i < n; i++)
+        result *= x;
+    printf("%d^%d = %lld\n", x, n, result);
+    return 0;
+}
